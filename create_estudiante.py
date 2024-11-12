8ae698f31bf7cadc2d4450cb33cee8444624bc41
egomezv19 <enzoo.g.17@gmail.com>
2024-11-11 20:47:01 -0500

    axdf

---
+++ create_estudiante.py
@@ -0,0 +1,30 @@
+import boto3
+
+def lambda_handler(event, context):
+    # Obtener los datos del evento
+    universidad = event['body']['universidad']
+    dni = event['body']['dni']
+    id_estudiante = event['body']['id_estudiante']
+    fecha_nacimiento = event['body']['fecha_nacimiento']
+    carrera = event['body']['carrera']
+    
+    # Inicializar el recurso de DynamoDB
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('estudiantes')
+    
+    # Crear el ítem para DynamoDB
+    estudiante = {
+        'universidad': universidad,
+        'dni': dni,
+        'id_estudiante': id_estudiante,
+        'fecha_nacimiento': fecha_nacimiento,
+        'carrera': carrera
+    }
+    # Insertar el ítem en DynamoDB
+    response = table.put_item(Item=estudiante)
+    
+    # Retornar la respuesta
+    return {
+        'statusCode': 200,
+        'body': response
+    }
